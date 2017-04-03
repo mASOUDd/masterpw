8d8c55db4a121e653f85981802d13e5b9679367a
Masoud Naservand <relive.mn@gmail.com>
2017-04-04 02:17:57 +0430

    RIP sha1

--- masterpw.py
+++ masterpw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from hashlib import sha1
+from hashlib import sha512
 import getpass
 
 # get master password
@@ -9,9 +9,9 @@ p = getpass.getpass('Master password: ')
 url = input('The url: ')
 
 # hash of master password
-h = sha1(p.encode()).hexdigest()
+h = sha512(p.encode()).hexdigest()
 
 # specific password for this url
 # 'ehem': because why not
-sp = sha1((h + 'ehem' + url + h).encode()).hexdigest()
+sp = sha512((h + 'ehem' + url + h).encode()).hexdigest()
 print('Password: {}'.format(sp))
